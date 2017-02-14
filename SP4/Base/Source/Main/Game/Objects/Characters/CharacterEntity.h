034fd50aa0835b57f6d9988e9b536a45bb17fe4b
unknown <153782U@M615G401.sidm.student.local>
2017-02-14 10:20:46 +0800

    Hope it works

---
+++ SP4/Base/Source/Main/Game/Objects/Characters/CharacterEntity.h
@@ -0,0 +1,33 @@
+#ifndef CHARACTER_ENTITY_H
+#define CHARACTER_ENTITY_H
+
+#include "../../Base/Source/Main/Engine/Objects/BaseObject.h"
+
+
+class CharacterEntity : public BaseObject
+{
+public:
+	CharacterEntity();
+	~CharacterEntity();
+
+private:
+
+
+};
+
+CharacterEntity::CharacterEntity()
+{
+}
+
+CharacterEntity::~CharacterEntity()
+{
+}
+
+
+
+
+
+
+
+
+#endif

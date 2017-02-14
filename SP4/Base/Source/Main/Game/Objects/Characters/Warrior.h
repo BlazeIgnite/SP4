83e72dd8df45956b9fafa6f0d7e24ef3603b2583
LOGIN <EMAIL>
2017-02-14 14:41:16 +0800

    Added in Base Character stuff

---
+++ SP4/Base/Source/Main/Game/Objects/Characters/Warrior.h
@@ -0,0 +1,17 @@
+#ifndef WARRIOR_H_
+#define WARRIOR_H_
+#include "CharacterEntity.h"
+
+
+class Warrior : public CharacterEntity
+{
+private:
+
+public:
+	Warrior();
+	~Warrior();
+};
+
+
+
+#endif
